a6de0bf0b7d5eb4d71729458475299985ac4eac4
konstantin <kosvadmail@atomlc.com.ua>
2022-05-02 11:52:51 +0200

    Добавляет svg спрайт и PixelPerfect

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Sandwich menu
 let mainNavSandwich = document.querySelector('.main-header__toggle');
 let mainNav = document.querySelector('.main-nav');
 
@@ -20,4 +21,34 @@ mainNavSandwich.addEventListener('click', function () {
     mainNav.classList.remove('main-nav--opened')
     mainNav.classList.add('main-nav--closed');
   }
+});
+
+// User-history-slider (mobile)
+let imageAfter = document.querySelector('.compare__image--after');
+let imageBefore = document.querySelector('.compare__image--before');
+let sliderButton = document.querySelector('.toggle__button');
+
+sliderButton.addEventListener('click', function () {
+  if(imageAfter.classList.contains('compare__image--hide')) {
+    sliderButton.classList.add('toggle__button--after');
+
+    imageAfter.classList.remove('compare__image--hide')
+    imageBefore.classList.add('compare__image--hide');
+  } else {
+    sliderButton.classList.remove('toggle__button--after');
+
+    imageBefore.classList.remove('compare__image--hide')
+    imageAfter.classList.add('compare__image--hide');
+  }
+});
+
+
+// User-history (tablet + desktop)
+let beforeContainer = document.querySelector('.compare__image-before-container');
+let afterContainer = document.querySelector('.compare__image-after-conatiner');
+let slider = document.querySelector('.toggle__input');
+
+slider.addEventListener('input', function() {
+  beforeContainer.style.width = slider.value + '%';
+  afterContainer.style.width = 100 - slider.value + '%';
 });
